f65700a5a7438a028acae42d7027bfb48bcd2317
Enrich404 <enrich404@users.noreply.github.com>
2026-06-23 01:52:19 -0300

    feat: route guide pages

--- proyecto_semestre_2/routes/web.php
+++ proyecto_semestre_2/routes/web.php
@@ -1,20 +1,28 @@
 <?php
 
-use App\Http\Controllers\EquipoController;
+use App\Http\Controllers\GuideController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
-Route::resource('equipos', EquipoController::class);
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/guias', [GuideController::class, 'index'])->name('guides.index');
+Route::get('/guias/{guide}', [GuideController::class, 'show'])->name('guides.show');
+
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return redirect()->route('guides.mine');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::get('/mis-guias', [GuideController::class, 'mine'])->name('guides.mine');
+    Route::get('/guias/crear/nueva', [GuideController::class, 'create'])->name('guides.create');
+    Route::post('/guias', [GuideController::class, 'store'])->name('guides.store');
+    Route::get('/guias/{guide}/editar', [GuideController::class, 'edit'])->name('guides.edit');
+    Route::patch('/guias/{guide}', [GuideController::class, 'update'])->name('guides.update');
+    Route::delete('/guias/{guide}', [GuideController::class, 'destroy'])->name('guides.destroy');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
